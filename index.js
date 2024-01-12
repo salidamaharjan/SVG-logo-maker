5a7036eeb1891094dbc1b46e2a6cf05dce20c3d8
salidamaharjan <salidamaharjan@outlook.com>
2024-01-12 09:09:10 -0600

    array of question added

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 //import inquirer
 const inquirer = require('inquirer');
+
+//Prepare a array of question
+const toIncludeInLogo = [{
+    name: "logo",
+    message: "Enter three character you want to include in logo"
+}]
+
