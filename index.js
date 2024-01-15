026ba5454a9b9bf9dec51c9aec501f5c58861175
salidamaharjan <salidamaharjan@outlook.com>
2024-01-15 17:36:50 -0600

    Parent class and child class constructed

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 //import inquirer
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 
 //Prepare a array of question
-const toIncludeInLogo = [{
+const toIncludeInLogo = [
+  {
     name: "logo",
-    message: "Enter three character you want to include in logo"
-}]
+    message: "Enter three character you want to include in logo",
+  },
+  {
+    name: "textColor",
+    message: "Enter a color for the letters"
+  },
+  {
+    name: "shapes",
+    type: "list",
+    message: "Choose the shape for the logo",
+    choices: ["Circle", "Triangle", "Square"]
+  },
+  {
+    name: "shapeColor",
+    message: "Enter color for shape"
+  }
+];
 
-//question appear in command line 
-async function startMakingLogo() {
-    const displayQUestion = await inquirer.prompt(toIncludeInLogo);
+//question appear in command line
+async function startMakingLogo(response) {
+  const displayQUestion = await inquirer.prompt(toIncludeInLogo);
 }
 
 //function called to initialize the app
