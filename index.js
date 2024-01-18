2ecd676208aac658134b63fa94a0c524ffa65752
salidamaharjan <salidamaharjan@outlook.com>
2024-01-17 23:41:43 -0600

    revised the code

--- index.js
+++ index.js
@@ -3,51 +3,69 @@ const inquirer = require("inquirer");
 //import object using object destructuring
 const { Circle, Triangle, Square } = require("./lib/shapes");
 //import file system
-const fs = require('fs/promises');
+const fs = require("fs/promises");
 
-//Prepare a array of question
-const toIncludeInLogo = [
-  {
-    name: "word",
-    message: "Enter three character you want to include in logo",
-  },
-  {
-    name: "textColor",
-    message: "Enter a color for the letters"
-  },
-  {
-    name: "shapes",
-    type: "list",
-    message: "Choose the shape for the logo",
-    choices: ["Circle", "Triangle", "Square"]
-  },
-  {
-    name: "shapeColor",
-    message: "Enter color for shape"
-  }
-];
+/**
+ * Prepares an array of question
+ * 
+ * @returns {Promise}
+ */
+async function getAnswers() {
+  const toIncludeInLogo = [
+    {
+      name: "text",
+      message: "Enter three character you want to include in logo",
+    },
+    {
+      name: "textColor",
+      message: "Enter a color for the letters",
+    },
+    {
+      name: "shape",
+      type: "list",
+      message: "Choose the shape for the logo",
+      choices: ["Circle", "Triangle", "Square"],
+    },
+    {
+      name: "shapeColor",
+      message: "Enter color for shape",
+    },
+  ];
+  return await inquirer.prompt(toIncludeInLogo);
+}
 
 //question appear in command line
-async function startMakingLogo(response) {
-  const displayQUestion = await inquirer.prompt(toIncludeInLogo);
+async function startMakingLogo() {
+  const displayQUestion = await getAnswers();
   let shape;
   //using switch case to capture the word, shape and color for the logo according to user input
-  switch (displayQUestion.shapes) {
-    case 'Circle':
-      shape = new Circle(displayQUestion.word, displayQUestion.shapeColor, displayQUestion.textColor);
+  switch (displayQUestion.shape) {
+    case "Circle":
+      shape = new Circle(
+        displayQUestion.text,
+        displayQUestion.shapeColor,
+        displayQUestion.textColor
+      );
       break;
-    case 'Triangle':
-      shape = new Triangle(displayQUestion.word, displayQUestion.shapeColor, displayQUestion.textColor);
+    case "Triangle":
+      shape = new Triangle(
+        displayQUestion.text,
+        displayQUestion.shapeColor,
+        displayQUestion.textColor
+      );
       break;
-    case 'Square':
-      shape = new Square(displayQUestion.word, displayQUestion.shapeColor, displayQUestion.textColor);
+    case "Square":
+      shape = new Square(
+        displayQUestion.text,
+        displayQUestion.shapeColor,
+        displayQUestion.textColor
+      );
       break;
   }
   //data is stored in logo.svg file on hte basis of user answer
-  await fs.writeFile('logo.svg', shape.draw());
-  console.log("Logo Created");
+  await fs.writeFile("logo.svg", shape.draw());
+  console.log("Generated logo.svg");
 }
-
 
 //function called to initialize the app
 startMakingLogo();
